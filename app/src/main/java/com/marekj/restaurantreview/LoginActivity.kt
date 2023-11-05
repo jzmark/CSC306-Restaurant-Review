8c4f1368e7334f6e379f4a875ddeca962ab41d11
Marek <56512676+jzmark@users.noreply.github.com>
2023-11-05 22:15:22 +0000

    RecyclerView complete

--- app/src/main/java/com/marekj/restaurantreview/LoginActivity.kt
+++ app/src/main/java/com/marekj/restaurantreview/LoginActivity.kt
@@ -1,11 +1,45 @@
 package com.marekj.restaurantreview
 
+import com.marekj.restaurantreview.recyclerview.RestaurantListAdapter
+import com.marekj.restaurantreview.recyclerview.RecyclerViewModel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_menu)
+        setContentView(R.layout.main_menu)
+
+        val imageModelArrayList = populateList()
+
+        val recyclerView = findViewById<View>(R.id.my_recycler_view) as RecyclerView // Bind to the recyclerview in the layout
+        val layoutManager = LinearLayoutManager(this) // Get the layout manager
+        recyclerView.layoutManager = layoutManager
+        val mAdapter = RestaurantListAdapter(imageModelArrayList)
+        recyclerView.adapter = mAdapter
+    }
+
+    /*
+     * A function to add names of F1 teams and logos to a list of MyModel
+     */
+    private fun populateList(): ArrayList<RecyclerViewModel> {
+        val list = ArrayList<RecyclerViewModel>()
+        val myImageList = arrayOf(R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant,
+            R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant,
+            R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant)
+        val myImageNameList = arrayOf(R.string.restaurant_name, R.string.restaurant_name,
+            R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name,
+            R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name)
+
+        for (i in 0..9) {
+            val imageModel = RecyclerViewModel()
+            imageModel.setNames(getString(myImageNameList[i]))
+            imageModel.setImages(myImageList[i])
+            list.add(imageModel)
+        }
+        return list
     }
 }
