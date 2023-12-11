4194285d13e00f65331011560f9d2b3856d18653
Marek <56512676+jzmark@users.noreply.github.com>
2023-12-11 14:53:39 +0000

    Fixed nav drawer and UI improvements

--- app/src/main/java/com/marekj/restaurantreview/LoginActivity.kt
+++ app/src/main/java/com/marekj/restaurantreview/LoginActivity.kt
@@ -1,45 +1,41 @@
 package com.marekj.restaurantreview
 
-import com.marekj.restaurantreview.recyclerview.RestaurantListAdapter
-import com.marekj.restaurantreview.recyclerview.RecyclerViewModel
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+//    private val signInLauncher = registerForActivityResult(
+//        FirebaseAuthUIActivityResultContract(),
+//    ) { res ->
+//        this.onSignInResult(res)
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.add_review)
+        setContentView(R.layout.login_menu)
 
-        /*val imageModelArrayList = populateList()
 
-        val recyclerView = findViewById<View>(R.id.my_recycler_view) as RecyclerView // Bind to the recyclerview in the layout
-        val layoutManager = LinearLayoutManager(this) // Get the layout manager
-        recyclerView.layoutManager = layoutManager
-        val mAdapter = RestaurantListAdapter(imageModelArrayList)
-        recyclerView.adapter = mAdapter*/
-    }
 
-    /*
-     * A function to add names of F1 teams and logos to a list of MyModel
-     */
-    private fun populateList(): ArrayList<RecyclerViewModel> {
-        val list = ArrayList<RecyclerViewModel>()
-        val myImageList = arrayOf(R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant,
-            R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant,
-            R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant)
-        val myImageNameList = arrayOf(R.string.restaurant_name, R.string.restaurant_name,
-            R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name,
-            R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name)
 
-        for (i in 0..9) {
-            val imageModel = RecyclerViewModel()
-            imageModel.setNames(getString(myImageNameList[i]) + " " + i)
-            imageModel.setImages(myImageList[i])
-            list.add(imageModel)
+
+
+
+
+
+        val loginButton = findViewById<Button>(R.id.signUp)
+
+
+
+
+
+
+
+
+        loginButton.setOnClickListener() {
+            val menuIntent = Intent(this, RestaurantListActivity::class.java)
+            startActivity(menuIntent)
         }
-        return list
     }
 }
--- app/src/main/java/com/marekj/restaurantreview/RestaurantListActivity.kt
+++ app/src/main/java/com/marekj/restaurantreview/RestaurantListActivity.kt
@@ -1,7 +1,71 @@
 package com.marekj.restaurantreview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
+import com.marekj.restaurantreview.recyclerview.RecyclerViewModel
+import com.marekj.restaurantreview.recyclerview.RestaurantListAdapter
 
 class RestaurantListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_menu)
+        drawerListener()
+
+        val imageModelArrayList = populateList()
+
+        val recyclerView = findViewById<View>(R.id.my_recycler_view) as RecyclerView // Bind to the recyclerview in the layout
+        val layoutManager = LinearLayoutManager(this) // Get the layout manager
+        recyclerView.layoutManager = layoutManager
+        val mAdapter = RestaurantListAdapter(imageModelArrayList)
+        recyclerView.adapter = mAdapter
+    }
+
+    private fun drawerListener() {
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.open()
+        }
+        val navigationView = findViewById<NavigationView>(R.id.navigation)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+            menuItem.isChecked = true
+            drawerLayout.close()
+            true
+//            if (menuItem.itemId == R.id.logoutDrawer) {
+//                val menuIntent = Intent(this, RestaurantListActivity::class.java)
+//                startActivity(menuIntent)
+//                true
+//            } else {
+//                val menuIntent = Intent(this, LoginActivity::class.java)
+//                startActivity(menuIntent)
+//                true
+//            }
+        }
+    }
+
+    private fun populateList(): ArrayList<RecyclerViewModel> {
+        val list = ArrayList<RecyclerViewModel>()
+        val myImageList = arrayOf(R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant,
+            R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant,
+            R.drawable.restaurant, R.drawable.restaurant, R.drawable.restaurant)
+        val myImageNameList = arrayOf(R.string.restaurant_name, R.string.restaurant_name,
+            R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name,
+            R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name, R.string.restaurant_name)
+
+        for (i in 0..9) {
+            val imageModel = RecyclerViewModel()
+            imageModel.setNames(getString(myImageNameList[i]) + " " + i)
+            imageModel.setImages(myImageList[i])
+            list.add(imageModel)
+        }
+        return list
+    }
 }
