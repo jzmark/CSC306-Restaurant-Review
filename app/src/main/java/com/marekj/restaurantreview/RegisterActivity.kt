3899a181c500228b78699db7a8a9f0a0aade968d
Marek <56512676+jzmark@users.noreply.github.com>
2023-12-12 21:04:38 +0000

    Logging in and signing up implemented

--- app/src/main/java/com/marekj/restaurantreview/RegisterActivity.kt
+++ app/src/main/java/com/marekj/restaurantreview/RegisterActivity.kt
@@ -8,15 +8,18 @@ import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.userProfileChangeRequest
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -29,7 +32,6 @@ class RegisterActivity : AppCompatActivity() {
     }
     public override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         if (currentUser != null) {
             //reload()
@@ -38,7 +40,7 @@ class RegisterActivity : AppCompatActivity() {
     private fun signUpListener() {
         val signUpBtn = findViewById<Button>(R.id.signUp)
         signUpBtn.setOnClickListener() {
-            val firstNameField : String? = findViewById<TextInputEditText>(R.id.nameInputField)
+            val usernameField : String? = findViewById<TextInputEditText>(R.id.usernameField)
                 .text.toString().trim()
             val emailField : String? = findViewById<TextInputEditText>(R.id.emailInputField)
                 .text.toString().trim()
@@ -46,35 +48,32 @@ class RegisterActivity : AppCompatActivity() {
                 .text.toString().trim()
             val pass2Field : String? = findViewById<TextInputEditText>(R.id.pass2InputField)
                 .text.toString().trim()
-            val checkState : Int = checkFields(firstNameField, emailField, pass1Field, pass2Field)
+            val checkState : Int = checkFields(usernameField, emailField, pass1Field, pass2Field)
             if (checkState == 0) {
                 auth.createUserWithEmailAndPassword(
-                    emailField.toString(),
-                    pass1Field.toString())
+                    emailField!!,
+                    pass1Field!!)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success")
-                            val user = auth.currentUser
+                            val user = Firebase.auth.currentUser
                             val profileUpdates = userProfileChangeRequest {
-                                displayName = firstNameField
+                                displayName = usernameField
                             }
-
                             user!!.updateProfile(profileUpdates)
                                 .addOnCompleteListener { task ->
                                     if (task.isSuccessful) {
                                         Log.d(TAG, "User profile updated.")
+                                        val loginActivity = Intent(this, LoginActivity::class.java)
+                                        startActivity(loginActivity)
+                                        finish()
                                     }
                                 }
-                            val loginActivity = Intent(this, LoginActivity::class.java)
-                            startActivity(loginActivity)
-                            finish()
                         } else {
-                            // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.exception)
                             val signUpBtn = findViewById<View>(R.id.signUp)
                             signUpBtn.hideKeyboard()
-                            Snackbar.make(signUpBtn, "TEST",
+                            Snackbar.make(signUpBtn, getString(R.string.emailExists),
                                 Snackbar.LENGTH_SHORT)
                                 .show()
                         }
@@ -87,12 +86,12 @@ class RegisterActivity : AppCompatActivity() {
         inputManager.hideSoftInputFromWindow(windowToken, 0)
     }
 
-    //Returns: 0 - no errors, 1 - wrong email format, 2 - not matching passwords, 3 - first name exceeds
-    //30 characters, 4 empty field, 5 pass length shorter than 6 characters
-    private fun checkFields(firstName : String?, email : String?, pass1 : String?,
+    //Returns: 0 - no errors, 1 - wrong email format, 2 - not matching passwords, 3 - username exceeds
+    //10 characters or smaller than 4, 4 empty field, 5 pass length shorter than 6 characters
+    private fun checkFields(username : String?, email : String?, pass1 : String?,
                             pass2 : String?) : Int {
         val signUpBtn = findViewById<View>(R.id.signUp)
-        if (firstName.isNullOrBlank() || email.isNullOrBlank() ||
+        if (username.isNullOrBlank() || email.isNullOrBlank() ||
             pass1.isNullOrBlank() || pass2.isNullOrBlank()) {
             signUpBtn.hideKeyboard()
             Snackbar.make(signUpBtn, getString(R.string.emptyField),
@@ -100,7 +99,7 @@ class RegisterActivity : AppCompatActivity() {
                 .show()
             return 4
         }
-        else if (firstName.length > 30) {
+        else if (username.length > 10 || username.length < 4) {
             signUpBtn.hideKeyboard()
             Snackbar.make(signUpBtn, getString(R.string.nameLimitExceeded),
                 Snackbar.LENGTH_SHORT)
--- app/src/main/java/com/marekj/restaurantreview/RestaurantListActivity.kt
+++ app/src/main/java/com/marekj/restaurantreview/RestaurantListActivity.kt
@@ -12,11 +12,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.marekj.restaurantreview.recyclerview.RecyclerViewModel
 import com.marekj.restaurantreview.recyclerview.RestaurantListAdapter
+import org.w3c.dom.Text
 
 class RestaurantListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,21 +26,30 @@ class RestaurantListActivity : AppCompatActivity() {
         setContentView(R.layout.main_menu)
         drawerListener()
 
+        
         val imageModelArrayList = populateList()
-
         val recyclerView = findViewById<View>(R.id.my_recycler_view) as RecyclerView // Bind to the recyclerview in the layout
         val layoutManager = LinearLayoutManager(this) // Get the layout manager
         recyclerView.layoutManager = layoutManager
         val mAdapter = RestaurantListAdapter(imageModelArrayList)
         recyclerView.adapter = mAdapter
+
+        val user = Firebase.auth.currentUser
+        user?.let {
+           var name = it.displayName
+            Snackbar.make(recyclerView, name.toString(),
+                Snackbar.LENGTH_SHORT)
+                .show()
+        }
     }
 
     private fun drawerListener() {
+        val headerDrawer = findViewById<NavigationView>(R.id.navigation)
+            .getHeaderView(0)
+            .findViewById<TextView>(R.id.headerNavDrawer)
         val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-        val headerTopNavDrawer = findViewById<TextView>(R.id.headerNavDrawer)
-        //headerTopNavDrawer.text = "dd"
-        Log.w(TAG, "Nazwa profilu " + getProfileName())
+        headerDrawer.text = getProfileName()
         topAppBar.setNavigationOnClickListener {
             drawerLayout.open()
         }
